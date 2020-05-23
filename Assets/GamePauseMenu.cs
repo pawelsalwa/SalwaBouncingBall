7b401e954fd749d62ac984911e25af23ea97bbb6
pawelsalwa <pawelmareksalwa@gmail.com>
2020-05-23 14:37:07 +0200

    *:better restart

--- Assets/GamePauseMenu.cs
+++ Assets/GamePauseMenu.cs
@@ -4,42 +4,45 @@ using UnityEngine.EventSystems;
 
 public class GamePauseMenu : UIBehaviour
 {
-    [SerializeField] private PauseGameEvent pauseGameEvent = null;
-    [SerializeField] private GameObject container = null;
+	[SerializeField] private PauseGameEvent pauseGameEvent = null;
+	[SerializeField] private GameObject container = null;
 
-    protected override void Start()
-    {
-        container.SetActive(false);
-    }
+	protected override void Start()
+	{
+		pauseGameEvent.UnpauseGame();
+		container.SetActive(false);
+	}
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape)) // shouldnt be here but theres no input system yet in the project
-        {
-            if (pauseGameEvent.Paused) pauseGameEvent.UnpauseGame();
-            else pauseGameEvent.PauseGame();
-        }
-    }
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape)) // shouldnt be here but theres no input system yet in the project
+		{
+			if (pauseGameEvent.Paused) pauseGameEvent.UnpauseGame();
+			else pauseGameEvent.PauseGame();
+		}
+	}
 
-    private void OnPause()
-    {
-        container.SetActive(true);
-    }
+	private void OnPause()
+	{
+		container.SetActive(true);
+	}
 
-    private void OnUnpause()
-    {
-        container.SetActive(false);
-    }
+	private void OnUnpause()
+	{
+		// Debug.Log($"<color=white>container == null : {container== null}</color>", container);
+		if (container != null)
+			container.SetActive(false);
+	}
 
-    protected override void OnEnable()
-    {
-        pauseGameEvent.SubscribeToPauseEvent(OnPause);
-        pauseGameEvent.SubscribeToUnpauseEvent(OnUnpause);
-    }
+	protected override void OnEnable()
+	{
+		pauseGameEvent.SubscribeToPauseEvent(OnPause);
+		pauseGameEvent.SubscribeToUnpauseEvent(OnUnpause);
+	}
 
-    protected override void OnDisable()
-    {
-        pauseGameEvent.UnSubscribeFromPauseEvent(OnPause);
-        pauseGameEvent.UnSubscribeFromUnpauseEvent(OnUnpause);
-    }
+	protected override void OnDisable()
+	{
+		pauseGameEvent.UnSubscribeFromPauseEvent(OnPause);
+		pauseGameEvent.UnSubscribeFromUnpauseEvent(OnUnpause);
+	}
 }
--- Assets/PauseGameEvent.cs
+++ Assets/PauseGameEvent.cs
@@ -56,6 +56,7 @@ namespace ScriptableObjects.Events
 		{
 			pauseActions = new List<Action>();
 			unpauseActions = new List<Action>();
+			Paused = false;
 		}
 	}
 }
